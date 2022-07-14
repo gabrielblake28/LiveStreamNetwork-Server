b800e2efd37627b5d6857180418d9d55364748a1
Caleb Blake <cdblake31@gmail.com>
2022-07-14 03:55:12 +0000

    Dev/calebblake/add test service

--- index.ts
+++ index.ts
@@ -100,9 +100,19 @@ app.use("/search", SearchRouter);
 
 const testService = new TestService();
 
-console.time("CreateEvents");
-// testService.CreateEvents(10000);
-console.timeEnd("CreateEvents");
+// console.time("CreateEvents");
+// testService.DeleteEvents().then(() => {
+//     testService.DeleteUsers().then(() => {
+//         testService.CreateUsers(10000).then(() => {
+//             testService
+//                 .CreateEvents(10000, new Date("2023-08-01"), true)
+//                 .then(() => {
+//                     console.timeEnd("CreateEvents");
+//                 });
+//         });
+//     });
+// });
+
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
